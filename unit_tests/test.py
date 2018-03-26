8ca8c8ddf31ee7da59d81d3f883dd3c01b1de73e
joeliu <lq6414@aliyun.com>
2018-03-26 17:27:40 +0900

    joe getsource

--- unit_tests/test.py
+++ unit_tests/test.py
@@ -5,10 +5,46 @@
 # @File    : test.py
 
 
-from lib.excel import Excel
-from utils import settings
+import threading, time, random
 
-for _path, _sheetname in settings.SRC_DATA.items():
-    _excel = Excel(_path)
-    print(1)
+queue = []
 
+con = threading.Condition()
+
+class Producer(threading.Thread):
+    def run(self):
+        while True:
+            if con.acquire():
+                if len(queue) > 100:
+                    con.wait()
+                else:
+                    elem = random.randrange(100)
+                    queue.append(elem)
+                    print("Producer a elem {}, Now size is {}".format(elem, len(queue)))
+                    time.sleep(random.random())
+                    con.notify()
+                con.release()
+
+class Consumer(threading.Thread):
+    def run(self):
+        while True:
+            if con.acquire():
+                if len(queue) < 0:
+                    con.wait()
+                else:
+                    elem = queue.pop()
+                    print("Consumer a elem {}. Now size is {}".format(elem, len(queue)))
+                    time.sleep(random.random())
+                    con.notify()
+                con.release()
+
+def main():
+    for i in range(3):
+        Producer().start()
+
+    for i in range(2):
+        Consumer().start()
+
+
+if __name__ == '__main__':
+    main()
---
+++ views/add/getSourceMessage.py
@@ -0,0 +1,27 @@
+
+from lib.excel import Excel
+from utils import settings
+import pathlib
+
+
+
+
+for path, sheetname in settings.SRC_DATA.items():
+    srcpath = path
+    srcsheetname = sheetname
+
+excel = Excel(srcpath)
+dim_list = excel.get_dimensions(srcsheetname)
+strStart = dim_list[0]
+endStart = dim_list[1]
+
+source_data = excel.read_excel_by_pos(sheetname = srcsheetname)
+source_column_names = excel.get_column_names(sheetname = srcsheetname)
+new_dict = {}
+list_source = []
+for singledata in source_data:
+    new_dict = dict([(source_column_names[i],singledata[i] ) for i in range(len(source_column_names))])
+    list_source.append(new_dict)
+
+for myiter in  list_source:
+    print(myiter)
