ceb5eff9c6e5821fb577602565193233818a5483
joeliu <lq6414@aliyun.com>
2018-04-03 15:03:36 +0900

    joe update cell update

--- views/delete/getCompareColName.py
+++ views/delete/getCompareColName.py
@@ -4,7 +4,7 @@ from lib.logger import StreamFileLogger
 
 _sflogger = StreamFileLogger(settings.LOG_FILE, __file__).get_logger()
 
-def get_match_columns(sheetname):
+def get_match_columns(sheetname,idx=None):
     _srcpath = settings.SRC_FILE_PATH
     _tgtpath = settings.TGT_FILE_PATH
     _srcexcel = Excel(_srcpath)
@@ -15,8 +15,13 @@ def get_match_columns(sheetname):
     for _item in _srccolumn:
         if _item in _tgtcolumn and _item is not None:
             _matchcolumn.append(_item)
+    if idx is not None:
+        for i in _matchcolumn:
+            if i in idx:
+                _matchcolumn.remove(i)
 
     _sflogger.info('matched column: {}'.format(_matchcolumn))
+
     return _matchcolumn
 
 def get_del_columns(sheetname):
@@ -27,6 +32,7 @@ def get_del_columns(sheetname):
     for _item in _match_columns:
         if _item in _srccolumn and _item is not None:
             _srccolumn.remove(_item)
+
     _sflogger.info('deleted column: {}'.format(_srccolumn))
     return _srccolumn
 
@@ -41,4 +47,38 @@ def get_add_columns(sheetname):
     _sflogger.info('added column: {}'.format(_tgtcolumn))
     return  _tgtcolumn
 
+def get_compare_colNum(sheetname,idx):
+    _srcpath = settings.SRC_FILE_PATH
+    _tgtpath = settings.TGT_FILE_PATH
+    _srcexcel = Excel(_srcpath)
+    _tgtexcel = Excel(_tgtpath)
+    _srccolumn = _srcexcel.get_column_names(sheetname)
+    _tgtcolumn = _tgtexcel.get_column_names(sheetname)
 
+    #getcompare column position for both sides
+    _matchcolumn = []
+    for _item in _srccolumn:
+        if _item in _tgtcolumn and _item is not None:
+            _matchcolumn.append(_item)
+    for _item in _matchcolumn:
+        if _item in idx:
+            _matchcolumn.remove(_item)
+    _sheadnum = []
+    _theadnum = []
+    for _maccol in _matchcolumn:
+        _curshead = _srcexcel.convert_col2header(sheetname, _maccol)
+        _sheadnum.append(_curshead)
+    for _maccol in _matchcolumn:
+        _curthead = _tgtexcel.convert_col2header(sheetname, _maccol)
+        _theadnum.append(_curthead)
+    _compareColZips = zip(_sheadnum, _theadnum)
+
+    #getcompare row position for both sides
+
+    return _compareColZips
+
+
+
+
+
+# get_compare_colNum('CAPS Industry KPIs New',['Name'])
--- views/delete/getMessageData.py
+++ views/delete/getMessageData.py
@@ -4,18 +4,28 @@ from lib.excel import Excel
 from lib.logger import StreamFileLogger
 
 _sflogger = StreamFileLogger(settings.LOG_FILE, __file__).get_logger()
-def get_data_message(path,sheetname):
+def get_data_message(path,sheetname,idx=None):
+
+    #initial index into list
 
     _excel = Excel(path)
-    _mactch_column_name = compareData.get_match_columns(sheetname)
-    if _mactch_column_name :
-        _headername = []
+    if idx is None:
+        _mactch_column_name = compareData.get_match_columns(sheetname)
+    else:
+        _indexCols = idx.split(',')
+        _mactch_column_name = compareData.get_match_columns(sheetname, _indexCols)
+
+    _headername = []
+    if idx is  None :
         for columnname in _mactch_column_name:
             _curcells = _excel.convert_col2header(sheetname,columnname)
             _headername.append(_curcells)
 
     else:
-        ''
+        for columnname in _indexCols:
+            _curcells = _excel.convert_col2header(sheetname,columnname)
+            _headername.append(_curcells)
+
     _cursheet = _excel.get_sheet(sheetname)
 
     _alldata = []
@@ -27,16 +37,19 @@ def get_data_message(path,sheetname):
             #get current cell line number and line column
 
             _cellvalue = _cursheet[_cellname].value
-
-            _rowdata.append(str(_cellvalue).upper())
+            _cellvalue = str(_cellvalue).upper()
+            _rowdata.append(_cellvalue.strip())
             #upper all values
 
         _alldata.append(_rowdata)
 
     for _item in _alldata:
-        _getcount = _alldata.count(_item)
-        _item.append(_getcount)
-
+        if _item in _alldata:
+            _getcount = _alldata.count(_item)
+            _item.append(_getcount)
+    print(_alldata)
     return _alldata
 
-# get_data_message(path = 'C:/Users/jliu409/DataCompare/src_data/CAPS.xlsx',sheetname = 'CAPS Industry KPIs New')
+get_data_message('C:/Users/jliu409/DataCompare/src_data/CAPS.xlsx','CAPS Industry KPIs New','Name,Age')
+
+
