75197eac0e59918893023025f5aa4d53d280f53b
MiracleYoung <miracleyoung0723@gmail.com>
2018-04-04 16:23:02 +0800

    argparser

--- views/app.py
+++ views/app.py
@@ -6,30 +6,33 @@
 
 
 import argparse
-from utils import settings, qwe
+from utils import settings, config
+from views.delete.handleRowData import setBgColorRowIdx
 
+_desc = '''
+        The sheet name should like "Sheet1,Sheet2,Sheet3", use double quote include sheetnames
+        balabala
+    '''
+_parser = argparse.ArgumentParser(description=_desc)
+_parser.add_argument('--src', type=str, help='Source file')
+_parser.add_argument('--src-sheets', type=str, help='Source file sheets')
+_parser.add_argument('--tgt', type=str, help='Target file')
+_parser.add_argument('--tgt-sheets', type=str, help='Target file sheets')
+
+# _args = _parser.parse_args(
+#     ['--src', 'abc.xlsx', '--src-sheets', 'CAPS,APQC,3rdparty', '--tgt', 'def.xlsx', '--tgt-sheets', 'qwe,asd,zxc'])
+_args = _parser.parse_args()
+print(_args)
 
 def main():
-    # TODO
-    # command
-    pass
+    setBgColorRowIdx(_args.src, _args.src_sheets, _args.tgt, _args.tgt_sheets)
 
 
 if __name__ == '__main__':
-    _desc = '''
-            The sheet name should like "Sheet1,Sheet2,Sheet3", use double quote include sheetnames
-            balabala
-        '''
-    _parser = argparse.ArgumentParser(description=_desc)
-    _parser.add_argument('--src', type=str, help='Source file')
-    _parser.add_argument('--src-sheets', type=str, help='Source file sheets')
-    _parser.add_argument('--tgt', type=str, help='Target file')
-    _parser.add_argument('--tgt-sheets', type=str, help='Target file sheets')
 
-    _args = _parser.parse_args(['--src', 'abc.xlsx', '--src-sheets','CAPS,APQC,3rdparty','--tgt', 'def.xlsx','--tgt-sheets','qwe,asd,zxc'])
-    print(_args)
-    # setattr(config.Config, 'SRC_DATA', {_args.src: _args.src_sheets})
-    # setattr(config.Config, 'TGT_DATA', {_args.tgt: _args.tgt_sheets})
+    _config = config.Config()
+    setattr(_config, 'SRC_DATA', {_args.src: _args.src_sheets})
+    setattr(_config, 'TGT_DATA', {_args.tgt: _args.tgt_sheets})
 
     main()
 
